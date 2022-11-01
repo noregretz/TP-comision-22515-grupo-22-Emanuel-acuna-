9a0ed58ef13649d0af3d4212d96c43a3e531f9af
MANU <emanuel.alejandro.acuna@gmail.com>
2022-10-31 22:44:28 -0300

    enlace a una API desde el index

---
+++ gatitos.js
@@ -0,0 +1,37 @@
+const dogbtn = document.getElementById("dogbtn");
+const catbtn =  document.getElementById("catbtn");
+
+async function getDog(){
+    const resp = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data =  await resp.json();
+    console.log(data);
+    showdata(data.message);
+}
+// getDog();
+
+// function showdata(dogimage){
+//     document.getElementById("dog").innerHTML = `
+//     <img src="${dogimage}" alt="">
+//     `
+// }
+
+
+// dogbtn.addEventListener("click",()=>{
+// getDog();
+// });
+
+
+
+async function getCat(){
+    const resp = await fetch("https://aws.random.cat/meow");
+    const data =  await resp.json();
+    console.log(data);
+    showdataCat(data.file);
+}
+getCat();
+
+function showdataCat(catimage){
+    document.getElementById("cat").innerHTML = `
+    <img src="${catimage}" alt="">
+    `
+}
