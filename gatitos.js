372afbccda492289ff5844ee366a6b48a1cc9e9d
noregretz <116655860+noregretz@users.noreply.github.com>
2022-10-31 22:47:50 -0300

    Update gatitos.js

--- gatitos.js
+++ gatitos.js
@@ -1,13 +1,6 @@
 const dogbtn = document.getElementById("dogbtn");
 const catbtn =  document.getElementById("catbtn");
 
-async function getDog(){
-    const resp = await fetch("https://dog.ceo/api/breeds/image/random");
-    const data =  await resp.json();
-    console.log(data);
-    showdata(data.message);
-}
-
 async function getCat(){
     const resp = await fetch("https://aws.random.cat/meow");
     const data =  await resp.json();
